8cc273317a929c979be220dbc0dc3ccfbfd4bda0
Joshua Mitchell <joshuamerrittmitchell@gmail.com>
2025-05-21 14:40:21 -0400

    update for b

---
+++ Metabolomics2/step5_steroid_annotate.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import argparse
+from mass2chem.formula import calculate_formula_mass
+
+def calculate_derivative_masses(steroid_csv, group_mass, group_name):
+    steroids_df = pd.read_csv(steroid_csv)
+    derivatives = []
+
+    for _, row in steroids_df.iterrows():
+        formula = row['formula']
+        base_mass = calculate_formula_mass(formula)
+        for groups_added in range(0, 4):  # Updated to include 0 groups
+            total_mass = base_mass + groups_added * group_mass
+            derivative = {
+                'steroid': row['steroid'],
+                'groups_added': groups_added,
+                'group_name': group_name,
+                'derivative_mass': total_mass
+            }
+            derivatives.append(derivative)
+
+    return pd.DataFrame(derivatives)
+
+
+def annotate_features(feature_tsv, derivatives_df, ppm_tolerance=10):
+    features_df = pd.read_csv(feature_tsv, sep='\t')
+
+    mzml_cols_standards = [x for x in features_df.columns if "Standard" in x]
+    print(mzml_cols_standards)
+    annotations = []
+    annotation_df = []
+    for _, feature in features_df.iterrows():
+        mz = feature['mz']
+        matches = []
+
+        for _, derivative in derivatives_df.iterrows():
+            expected_mass = derivative['derivative_mass'] + 1.00727647 
+            tolerance = expected_mass * ppm_tolerance / 1e6
+            
+            if abs(mz - expected_mass) <= tolerance:
+                if all(feature[file] > 0 for file in mzml_cols_standards):
+                    match = f"{derivative['steroid']}+{derivative['groups_added']}{derivative['group_name']}"
+                    matches.append(match)
+                    print(match)
+        annotation_df.append({'mz': feature['mz'], 'id_number': feature['id_number'], 'annotation': ';'.join(matches)})
+    return pd.DataFrame(annotation_df)
+
+
+if __name__ == '__main__':
+    derivatives_df_dncl = calculate_derivative_masses('steroids.csv', 233.05105, "DnCl")
+    derivatives_df_dnhz = calculate_derivative_masses('steroids.csv', 247.07793, "DnHz")
+
+    annotated_df_dncl_pel = annotate_features("/Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/Metabolomics3/results/Metabolomics3DnCl_Pellet/output/feature_table.tsv", derivatives_df_dncl)
+    annotated_df_dnhz_pel = annotate_features("/Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/Metabolomics3/results/Metabolomics3DnHz_Pellet/output/feature_table.tsv", derivatives_df_dnhz)
+    annotated_df_dncl_sup = annotate_features("/Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/Metabolomics3/results/Metabolomics3DnCl_Supernatant/output/feature_table.tsv", derivatives_df_dncl)
+    annotated_df_dnhz_sup = annotate_features("/Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/Metabolomics3/results/Metabolomics3DnHz_Supernatant/output/feature_table.tsv", derivatives_df_dnhz)
+    
+    
+    annotated_df_dncl_pel.to_csv("/Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/Metabolomics3/results/Metabolomics3DnCl_Pellet/output/annotation_table.tsv", sep='\t', index=False)
+    annotated_df_dnhz_pel.to_csv("/Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/Metabolomics3/results/Metabolomics3DnHz_Pellet/output/annotation_table.tsv", sep='\t', index=False)
+    annotated_df_dncl_sup.to_csv("/Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/Metabolomics3/results/Metabolomics3DnCl_Supernatant/output/annotation_table.tsv", sep='\t', index=False)
+    annotated_df_dnhz_sup.to_csv("/Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/Metabolomics3/results/Metabolomics3DnHz_Supernatant/output/annotation_table.tsv", sep='\t', index=False)
--- Metabolomics3/step4_pcpfm_process.sh
+++ Metabolomics3/step4_pcpfm_process.sh
@@ -17,41 +17,46 @@ run() {
     #python3.11 -m pcpfm assemble -o $BASE -j $OUTPUT$MODE -s $CSV --filter=$FILTER --path_field mzml_path --name_field "File Name"
 
     if [[ $MODE =~ (DnHz|dnhz|DnCl|dncl) ]]; then
-        #echo "skip"
+        echo "skip"
         #python3.11 -m pcpfm assemble -o $BASE -j $OUTPUT$MODE -s $CSV --filter=$FILTER --path_field mzml_path --name_field "File Name"
         #python3.11 -m pcpfm asari -i $EXPERIMENT --extra_asari "--min_peak_height 500000 --min_intensity_threshold 500000"
-        python3.11 -m pcpfm build_empCpds -i $EXPERIMENT -tm preferred -em preferred --add_singletons=True
-        python3.11 -m pcpfm generate_output -i $EXPERIMENT -em preferred -tm preferred 
+        #python3.11 -m pcpfm build_empCpds -i $EXPERIMENT -tm preferred -em preferred --add_singletons=True
+        #python3.11 -m pcpfm generate_output -i $EXPERIMENT -em preferred -tm preferred 
     else
+        echo "2"
         #python3.11 -m pcpfm assemble -o $BASE -j $OUTPUT$MODE -s $CSV --filter=$FILTER --path_field mzml_path --name_field "File Name"
         #python3.11 -m pcpfm asari -i $EXPERIMENT
         #python3.11 -m pcpfm build_empCpds -i $EXPERIMENT -tm preferred -em preferred --add_singletons=True
         
-        #if [[ $MODE =~ .*hilicneg.* && $MODE =~ .*Supernatant.* ]]; then
-        #    python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/10052023_AcquireX_iPSC_WT_Supernatant/10052023_AcquireX_iPSC_WT_Supernatant_HILICneg
-        #fi
-        #if [[ $MODE =~ .*hilicpos.* && $MODE =~ .*Supernatant.* ]]; then
-        #    python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/10052023_AcquireX_iPSC_WT_Supernatant/10052023_AcquireX_iPSC_WT_Supernatant_HILICpos
-        #fi
-        #if [[ $MODE =~ .*rpneg.* && $MODE =~ .*Supernatant.* ]]; then
-        #    python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/10052023_AcquireX_iPSC_WT_Supernatant/10052023_AcquireX_iPSC_WT_Supernatant_RPneg
-        #fi
-        #if [[ $MODE =~ .*rppos.* && $MODE =~ .*Supernatant.* ]]; then
-        #    python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/10052023_AcquireX_iPSC_WT_Supernatant/10052023_AcquireX_iPSC_WT_Supernatant_RPpos/10052023_AcquireX_iPSC_WT_Supernatant_RPpos
-        #fi
+        if [[ $MODE =~ .*hilicneg.* && $MODE =~ .*Supernatant.* ]]; then
+            echo "4"
+            python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/AcquireX_Kolf2.1J_iPSC_WT/10052023_AcquireX_iPSC_WT_Supernatant/10052023_AcquireX_iPSC_WT_Supernatant_HILICneg
+        fi
+        if [[ $MODE =~ .*hilicpos.* && $MODE =~ .*Supernatant.* ]]; then
+            python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/AcquireX_Kolf2.1J_iPSC_WT/10052023_AcquireX_iPSC_WT_Supernatant/10052023_AcquireX_iPSC_WT_Supernatant_HILICpos
+        fi
+        if [[ $MODE =~ .*rpneg.* && $MODE =~ .*Supernatant.* ]]; then
+            python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/AcquireX_Kolf2.1J_iPSC_WT/10052023_AcquireX_iPSC_WT_Supernatant/10052023_AcquireX_iPSC_WT_Supernatant_RPneg
+        fi
+        if [[ $MODE =~ .*rppos.* && $MODE =~ .*Supernatant.* ]]; then
+            echo "5"
+            python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/AcquireX_Kolf2.1J_iPSC_WT/10052023_AcquireX_iPSC_WT_Supernatant/10052023_AcquireX_iPSC_WT_Supernatant_RPpos
+        fi
 
-        #if [[ $MODE =~ .*hilicneg.* && $MODE =~ .*Cellpellet.* ]]; then
-        #    python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/10112023_AcquireX_iPSC_WT_Cellpellets/10112023_AcquireX_iPSC_WT_Cellpellets_HILICneg
-        #fi
-        #if [[ $MODE =~ .*hilicpos.* && $MODE =~ .*Cellpellet.* ]]; then
-        #    python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/10112023_AcquireX_iPSC_WT_Cellpellets/10112023_AcquireX_iPSC_WT_Cellpellets_HILICpos
-        #fi
-        #if [[ $MODE =~ .*rpneg.* && $MODE =~ .*Cellpellet.* ]]; then
-        #    python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/10112023_AcquireX_iPSC_WT_Cellpellets/10112023_AcquireX_iPSC_WT_Cellpellets_RPneg
-        #fi
-        #if [[ $MODE =~ .*rppos.* && $MODE =~ .*Cellpellet.* ]]; then
-        #    python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/10112023_AcquireX_iPSC_WT_Cellpellets/10112023_AcquireX_iPSC_WT_Cellpellets_RPpos
-        #fi
+        if [[ $MODE =~ .*hilicneg.* && $MODE =~ .*Cellpellet.* ]]; then
+            echo "7"
+            python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/AcquireX_Kolf2.1J_iPSC_WT/10112023_AcquireX_iPSC_WT_Cellpellets/10112023_AcquireX_iPSC_WT_Cellpellets_HILICneg
+        fi
+        if [[ $MODE =~ .*hilicpos.* && $MODE =~ .*Cellpellet.* ]]; then
+            python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/AcquireX_Kolf2.1J_iPSC_WT/10112023_AcquireX_iPSC_WT_Cellpellets/10112023_AcquireX_iPSC_WT_Cellpellets_HILICpos
+        fi
+        if [[ $MODE =~ .*rpneg.* && $MODE =~ .*Cellpellet.* ]]; then
+            python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/AcquireX_Kolf2.1J_iPSC_WT/10112023_AcquireX_iPSC_WT_Cellpellets/10112023_AcquireX_iPSC_WT_Cellpellets_RPneg
+        fi
+        if [[ $MODE =~ .*rppos.* && $MODE =~ .*Cellpellet.* ]]; then
+            echo "8"
+            python3.11 -m pcpfm map_ms2 -i $EXPERIMENT -em preferred -nm preferred_map -tm preferred_w_MS2 --ms2_dir /Users/mitchjo/Downloads/MorPhiC_Data_Release_05_13_2025/AcquireX_Kolf2.1J_iPSC_WT/10112023_AcquireX_iPSC_WT_Cellpellets/10112023_AcquireX_iPSC_WT_Cellpellets_RPpos
+        fi
         python3.11 -m pcpfm l4_annotate -i $EXPERIMENT -em preferred_map -nm HMDB_LMSD_annotated_preferred
         python3.11 -m pcpfm l2_annotate -i $EXPERIMENT -em HMDB_LMSD_annotated_preferred -nm HMDB_LMSD_MoNA_annotated_preferred
         python3.11 -m pcpfm generate_output -i $EXPERIMENT -em HMDB_LMSD_MoNA_annotated_preferred -tm preferred 
